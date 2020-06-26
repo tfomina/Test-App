dc51f9ae793b7fdba52d02ac1ce29f41a0b1748a
Tatyana Fomina <tayna-anita@mail.ru>
2020-06-26 22:53:00 +0300

    Базовая сборка проекта

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.min.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "Test App",
+    }),
+  ],
+};
